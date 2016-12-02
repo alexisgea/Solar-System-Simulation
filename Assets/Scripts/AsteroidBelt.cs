3a50e18b9ef6fba612a695bfb6c1a37d5bdf5c15
Alexis <alexis.giard@gmail.com>
2016-12-02 21:02:07 +0100

    major refactoring of asteroid belt

--- Assets/Scripts/AsteroidBelt.cs
+++ Assets/Scripts/AsteroidBelt.cs
@@ -4,29 +4,23 @@ using System.Collections;
 public class AsteroidBelt : MonoBehaviour {
 
 	[SerializeField]
-	private Material _astMat;
-
+	private int _beltPopulation;
 	[SerializeField]
-	private int _fieldSize;
-
+	private int _maxRange;
 	[SerializeField]
-	private float _astRadius = 1f;
+	private int _minRange;
+	[SerializeField]
+	private float _inclinationRange;
+	[SerializeField]
+	private float _eccentricityRange;	
+	[SerializeField]
+	private float _asteroidRadius = 1f;
+	[SerializeField][Range(3,6)]
+	private int _asteroidComplexity = 3;
+	[SerializeField]
+	private Material _beltMaterial;
 
-	private Transform[] _astPos;
-	private float[] _astSemiMajAxis; // semi major axis in km
-	private float[] _astStartOrbitRot; // start position in radian
-	private float[] _astInclinaison; // orbit inclination in radian
-	private float[] _orbitPeriod;
-
-
-	private Vector3 _astPlaneOrigin  = new Vector3(0, 0, 0);
-	private Vector3 _astPlaneRight = new Vector3(1, 0, 0);
-	private Vector3 _astPlaneUp  = new Vector3(0, 1, 0);
-	private Vector3 _astPlaneBack  = new Vector3(0, 0, 1);
-	
-	private Vector2[] _vertOffset1;
-	private Vector2[] _vertOffset2;
-	private Vector2[] _vertOffset3;
+	private Asteroid[] _asteroids;
 
 	private float _timeScale;
 	private float _orbitScale;
@@ -38,96 +32,74 @@ public class AsteroidBelt : MonoBehaviour {
 	
 	// Use this for initialization
 	private void Start () {
-		
+
 		FindObjectOfType<StellarSystem>().Scaling += UpdateScale;
 		_orbitScale = FindObjectOfType<StellarSystem>().OrbitScale;
 		_timeScale = FindObjectOfType<StellarSystem>().TimeScale;
 
-		_astPos = new Transform[_fieldSize];
-		_astSemiMajAxis = new float[_fieldSize];
-		_astStartOrbitRot = new float[_fieldSize];
-		_astInclinaison = new float[_fieldSize];
-		_orbitPeriod = new float[_fieldSize];
-		_vertOffset1 = new Vector2[_fieldSize];
-		_vertOffset2 = new Vector2[_fieldSize];
-		_vertOffset3 = new Vector2[_fieldSize];
-
-		for (int j = 0; j < _fieldSize; j++) {
-			_astPos[j] = transform;
-			_astSemiMajAxis [j] = Random.Range (327800f, 476800f);
-			_astStartOrbitRot [j] = Mathf.Deg2Rad * Random.Range(0f, 360f);
-			_astInclinaison [j] = Mathf.Deg2Rad * Random.Range(-20f, 20f);
-			_orbitPeriod[j] = 2 * Mathf.PI * Mathf.Sqrt (Mathf.Pow (_astSemiMajAxis [j] * _orbitScale, 3) / (17.78f));
-			_vertOffset1 [j] = new Vector2(Random.Range(-_astRadius, _astRadius), Random.Range(-_astRadius, _astRadius));
-			_vertOffset2 [j] = new Vector2(Random.Range(-_astRadius, _astRadius), Random.Range(-_astRadius, _astRadius));
-			_vertOffset3 [j] = new Vector2(Random.Range(-_astRadius, _astRadius), Random.Range(-_astRadius, _astRadius));
-
-		}
+		GenerateBelt();
 
 	}
 	
-
 	private void Update() {
 		_totalTime += Time.deltaTime * _timeScale;
 	}
 
 	private void OnRenderObject() {
 
-		if (!_astMat) {
+		if (!_beltMaterial) {
 			Debug.LogError("Please assign asteroid belt material on the inspector");
 			return;
 		}
 
 		GL.PushMatrix();
-		_astMat.SetPass(0);
+		_beltMaterial.SetPass(0);
 
 		Vector3 camPosition = Camera.main.transform.position;
 		Quaternion camRotation = Camera.main.transform.rotation;
 		Plane[] frustumPlanes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
 
-		for (int j = 0; j < _fieldSize; j+=2) {
-			UpdateAsteroid(j, camPosition, camRotation, frustumPlanes);
-			UpdateAsteroid(j+1, camPosition, camRotation, frustumPlanes);
+		for (int i = 0; i < _beltPopulation; i+=2) {
+			UpdateAsteroid(i, camPosition, camRotation, frustumPlanes);
+			UpdateAsteroid(i+1, camPosition, camRotation, frustumPlanes);
 		}
 		GL.PopMatrix();
 
 	}
 
-	private void UpdateAsteroid(int j, Vector3 camPosition, Quaternion camRotation, Plane[] frustumPlanes){
-		_astPos[j].position = ComputeAstPos(j);
-		float scale = ComputeScale(j, camPosition);
-		Bounds asteroidBouds = new Bounds(_astPos[j].position, Vector3.one * _astRadius * scale);
-		
-		if(GeometryUtility.TestPlanesAABB(frustumPlanes, asteroidBouds))
-			DrawAsteroid (j, _astPos[j].position, camRotation, scale);
+	private void UpdateAsteroid(int i, Vector3 camPosition, Quaternion camRotation, Plane[] frustumPlanes){
+		Vector3 position = ComputeAsteroidPos(i);
+		float scale = ComputeScale(i, position, camPosition);
+		Bounds asteroidBounds = new Bounds(position, Vector3.one * _asteroidRadius * scale);
+
+		if(GeometryUtility.TestPlanesAABB(frustumPlanes, asteroidBounds))
+			DrawAsteroid (i, position, camRotation, scale);
 
 	}
 
-	private Vector3 ComputeAstPos(int j) {
-		//float T = 2 * Mathf.PI * Mathf.Sqrt (Mathf.Pow (_astSemiMajAxis [j] * _orbitScale, 3) / (17.78f));
-		float T = _orbitPeriod[j];
-		float rot = _astStartOrbitRot[j] + (_totalTime / T); // float rot = s[j] + (totalTime / T[j]);
+	private Vector3 ComputeAsteroidPos(int i) {
+		float rot = _asteroids[i].InitialArgument + (_totalTime / _asteroids[i].Period);
 		
-		float firstMember = _astSemiMajAxis[j] * _orbitScale * Mathf.Cos (rot);
-		float secondMember = _astSemiMajAxis[j] * _orbitScale * Mathf.Sin (rot);
-		Vector3 spacePos = _astPlaneOrigin + firstMember * _astPlaneRight + secondMember * _astPlaneBack;
+		float firstMember = _asteroids[i].SemiMajorAxis * _orbitScale * Mathf.Cos (-rot);
+		float secondMember = _asteroids[i].SemiMinorAxis * _orbitScale * Mathf.Sin (-rot);
+		Vector3 spacePos = _asteroids[i].OrbitFocus + firstMember * _asteroids[i].MajorVector + secondMember * _asteroids[i].MinorVector;
 
 		return spacePos;
 	}	
 
-	private float ComputeScale(int j, Vector3 camPos){
-		Vector3 diff = _astPos[j].position - camPos;
+	private float ComputeScale(int j, Vector3 position, Vector3 camPos){
+		Vector3 diff = position - camPos;
 		return diff.magnitude;
 	}
 
-	private void DrawAsteroid(int j, Vector3 astPos, Quaternion camRot, float scale) {
+	private void DrawAsteroid(int i, Vector3 astPos, Quaternion camRot, float scale) {
 		
 		// this step seems to take half the script time
 		// would it be possible to only rotate one asteroid and then base all the other on the same rotation?
 		//////////////////
-		_vert1 = astPos + (camRot * _vertOffset1[j] * scale);
-		_vert2 = astPos + (camRot * _vertOffset2[j] * scale);
-		_vert3 = astPos + (camRot * _vertOffset3[j] * scale);
+		_vert1 = astPos + (camRot * _asteroids[i].Shape[0] * scale);
+		_vert2 = astPos + (camRot * _asteroids[i].Shape[1] * scale);
+		_vert3 = astPos + (camRot * _asteroids[i].Shape[2] * scale);
 		//////////////////
 
 		GL.Begin(GL.TRIANGLES);
@@ -149,8 +121,8 @@ public class AsteroidBelt : MonoBehaviour {
 
 
 	private void RecomputePeriods(){
-		for (int j = 0; j < _fieldSize; j++)
-			_orbitPeriod[j] = 2 * Mathf.PI * Mathf.Sqrt (Mathf.Pow (_astSemiMajAxis [j] * _orbitScale, 3) / (17.78f));
+		for (int i = 0; i < _beltPopulation; i++)
+			_asteroids[i].Period = 2 * Mathf.PI * Mathf.Sqrt (Mathf.Pow (_asteroids[i].SemiMajorAxis * _orbitScale, 3) / (17.78f));
 	}
 
 	public void UpdateScale(string variable, float value) {
@@ -168,4 +140,78 @@ public class AsteroidBelt : MonoBehaviour {
                 break;
         }
     }
+
+	private void GenerateBelt(){
+
+		_asteroids = new Asteroid[_beltPopulation];
+
+		for (int i = 0; i < _beltPopulation; i++) {
+			int semiMajorAxis = Random.Range(_minRange, _maxRange);
+			float inclination = Random.Range(0f, _inclinationRange).RandomSign();
+			float eccentricity = Random.Range(0f, _eccentricityRange);
+
+			_asteroids[i] = AsteroidGenerator(semiMajorAxis, inclination, eccentricity, _asteroidComplexity, _asteroidRadius);
+		}
+
+		
+
+	}
+
+	private Asteroid AsteroidGenerator(int semiMajorAxis, float inclination = 0f, float eccentricity = 0f, int complexity = 3, float size = 1f){
+
+		// generate asteroid shape
+		Vector2[] shape = new Vector2[complexity];
+		for(int i=0; i<complexity; i++){
+			float x = Random.Range(size/2, size).RandomSign();
+			float y = Random.Range(size/2, size).RandomSign();
+			shape[i] = new Vector2(x, y);
+		}
+		
+		// orienting orbit plane // NOT WORKING
+		float perihelionArgument = Random.Range(0f, 2*Mathf.PI);
+		transform.Rotate(new Vector3(inclination, perihelionArgument, 0));
+
+		// getting transform parameters 
+		Vector3 forward = transform.forward;
+		Vector3 right = transform.right;
+		Vector3 center = semiMajorAxis * forward * eccentricity;
+
+		// getting missing position parameters
+		int semiMinorAxis = (int)(semiMajorAxis * Mathf.Sqrt(1 - Mathf.Pow(eccentricity, 2f)));
+		float initialArgument = Random.Range(0f, 2*Mathf.PI);
+		float period = 2 * Mathf.PI * Mathf.Sqrt (Mathf.Pow (semiMajorAxis * _orbitScale, 3) / (17.78f));  // forgot what was the 17.78f
+
+		// reseting base transform
+		transform.ResetTransformation();
+		
+		// generating asteroid
+		return new Asteroid(shape, center, forward, right, semiMajorAxis, semiMinorAxis, initialArgument, period);
+	}
+
 }
+
+
+class Asteroid {
+
+	public Vector2[] Shape {private set; get;}
+	public Vector3 OrbitFocus {private set; get;}
+	public Vector3 MajorVector {private set; get;}
+	public Vector3 MinorVector {private set; get;}
+	public int SemiMajorAxis {private set; get;}
+	public int SemiMinorAxis {private set; get;}
+	public float InitialArgument{private set; get;}
+	public float Period{set; get;}  // set should be private as well
+
+	public Asteroid(Vector2[] shape, Vector3 orbitFocus, Vector3 majorVector, Vector3 minorVector, int semiMajorAxis, int semiMinorAxis, float initialArgument, float period){
+		Shape = shape;
+		OrbitFocus = orbitFocus;
+		MajorVector = majorVector;
+		MinorVector = minorVector;
+		SemiMajorAxis = semiMajorAxis;
+		SemiMinorAxis = semiMinorAxis;
+		InitialArgument = initialArgument;
+		Period = period;
+	}
+
+}
+
---
+++ Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ExtensionMethods {
+
+	public static float RandomSign(this float number){
+		int sign = Random.value < .5? 1 : -1;
+		return sign * number;
+	}
+	
+	public static void ResetTransformation(this Transform trans)
+    {
+        trans.position = Vector3.zero;
+        trans.localRotation = Quaternion.identity;
+        trans.localScale = new Vector3(1, 1, 1);
+    }
+
+
+}
